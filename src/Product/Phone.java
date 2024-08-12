6f64a37ce9ce7e8e060ad56bda7bbb56117f9420
Nese <neseilhannn@gmail.com>
2024-08-12 18:11:08 +0300

    phone class.

--- src/Product/Phone.java
+++ src/Product/Phone.java
@@ -1,4 +1,28 @@
 package Product;
 
-public class Phone {
+public class Phone extends Product {
+    private int batteryCapacityInMh;
+    private String color;
+
+    public Phone (int id, double price, double discountRate, int stock, String name, Brand brand, int storage, int screenSize, int ram, int batteryCapacityInMah, String color) {
+        super(id, price, discountRate, stock, name, brand, storage, screenSize, ram);
+        this.batteryCapacityInMh = batteryCapacityInMah;
+        this.color = color;
+    }
+    public Phone () {
+
+    }
+    public int getBatteryCapacityInMh () {
+        return batteryCapacityInMh;
+    }
+
+    public String getColor () {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" %-5d Mah | %-12s |", batteryCapacityInMh, color);
+    }
+
 }
